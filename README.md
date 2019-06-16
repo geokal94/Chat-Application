5dd7489e51e534fdc554bcf7836d2b8e6314a764
George Kallis <giorgos.kallis.gr@gmail.com>
2019-06-16 12:50:14 +0300

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# chat-app
+# Chat Application
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    
     <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+
+.welcome{
+  max-width:400px;
+  margin-top:100px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.welcome h2{
+  font-size:3em;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.welcome button{
+  margin:30px auto;
 }
+
 </style>
